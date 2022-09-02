1b711d114969e63556c0afe120d768c6845d9362
ChocoLZS <chocoielzs@gmail.com>
2022-09-02 09:49:04 +0800

    update

--- tree/l98.ts
+++ tree/l98.ts
@@ -1,19 +1,29 @@
-function isValidBST(root: TreeNode | null): boolean {
-    let queue = new Array<number>();
-    _buildQueue(root, queue);
-    let flag = true;
-    if (queue.length == 1)
-        return flag;
-    for (let i = 0; i < queue.length - 1) {
-        flag = flag && (queue[i] < queue[i + 1]);
-    }
-    return flag
+// function isValidBST(root: TreeNode | null): boolean {
+//     let queue = new Array<number>();
+//     _buildQueue(root, queue);
+//     let flag = true;
+//     if (queue.length == 1)
+//         return flag;
+//     for (let i = 0; i < queue.length - 1) {
+//         flag = flag && (queue[i] < queue[i + 1]);
+//     }
+//     return flag
 
+// };
+
+// function _buildQueue(node: TreeNode | null, queue: Array<number>) {
+//     if (node == null) return;
+//     _buildQueue(node.left, queue);
+//     queue.push(node.val);
+//     _buildQueue(node.right, queue);
+// } 
+
+function isValidBST(root: TreeNode | null): boolean {
+    return helper(root, -Infinity, Infinity);
 };
 
-function _buildQueue(node: TreeNode | null, queue: Array<number>) {
-    if (node == null) return;
-    _buildQueue(node.left, queue);
-    queue.push(node.val);
-    _buildQueue(node.right, queue);
-} 
+function helper(root: TreeNode | null, lower: number, upper: number): boolean {
+    if (root == null) return true;
+    if (root.val <= lower || root.val >= upper) return false;
+    return helper(root.left, lower, root.val) && helper(root.right, root.val, upper);
+}
